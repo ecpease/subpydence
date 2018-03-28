69247a604080b08a01fca7cd0fd924b9e3e7fc52
rosskush <rosskushnereit@Rosss-MacBook-Air.local>
2018-03-27 20:04:37 -0500

    got rid of the need for adding the ttim model to the compaction outputs, still need to get 2d subscidence to work

--- subtim/compaction.py
+++ subtim/compaction.py
@@ -1,22 +1,24 @@
 import ttim
 from ttim.aquifer_parameters import param_3d, param_maq
 import numpy as np
-def NonDelay(ml,drawdown,z,LN,HC,Sfe,Sfv):#,Com,ComE,ComV):
-    tmin, tmax = ml.tmin, ml.tmax
-    print(tmin)
+
+
+def NonDelay(drawdown,z,LN,HC,Sfe,Sfv): #,Com,ComE,ComV):
+    """
+
+    :param drawdown:
+    :param z: elevations of each each geological formation, top, botm1, botm1 etc...
+    :param LN: Number of delay beds in each geological unit.
+    :param HC: preconsolodation head
+    :param Sfe: skeletol elastic storage coefficient
+    :param Sfv: skeletal inelastic storage coefficient
+    :return:
+    """
     z = np.array(z)
     nlay = len(z) - 1
-    LN_dict = {}
-    HC_dict = {}
-    for i in range(len(z)-1):
-        LN_dict[i] = LN[i]
-        HC_dict[i] = HC[i]
-
-
-    print(LN_dict)
-
     def del_Estress(drawdown,Pw=62.42796529, g=240177996288.):
         """
+
         theta = vertical effective stress (positive for increase)
         :param pw: 62.42796529 lb/ft^3
         :param g: 32.17405 ft/sec^2
@@ -29,12 +31,11 @@ def NonDelay(ml,drawdown,z,LN,HC,Sfe,Sfv):#,Com,ComE,ComV):
         return np.array(theta)
 
     theta = del_Estress(drawdown,Pw=62.42796529, g=240177996288.)
-
     pc_dd = []
     for lay in range(nlay):
         pc_dd = z[lay] - HC
 
-    theta_pc = del_Estress(drawdown=pc_dd,Pw=62.42796529, g=240177996288.) # prconsolidation
+    theta_pc = del_Estress(drawdown=pc_dd,Pw=62.42796529, g=240177996288.) # preconsolidation
     b = []
     for lay in range(nlay):
         for i in range(len(drawdown)):
@@ -43,26 +44,55 @@ def NonDelay(ml,drawdown,z,LN,HC,Sfe,Sfv):#,Com,ComE,ComV):
                     S_k = Sfe[lay][Sk]
                 elif theta[lay][i] >= theta_pc[lay]:
                     S_k = Sfv[lay][Sk]
-
-
                 b.append(S_k * drawdown[i])
-
     comp = []
-    # print(b[0])
     b = np.array(b)
-    # print(b.shape)
     for lay in range(nlay):
         comp.append(np.zeros((len(b[0]))))
         comp[lay] += b[lay] * LN[lay]
-        #+= np.array(b[lay]) #.sum(axis=0)
-        # for i in range(len(b[lay])):
-        #   comp[lay] += b[lay][i]
 
     return comp
 
+def NonDelayGrid(drawdownGrid,z,LN,HC,Sfe,Sfv): #,Com,ComE,ComV):
+    z = np.array(z)
+    nlay = len(z) - 1
+    def del_Estress(drawdownGrid, Pw=62.42796529, g=240177996288.):
+        """
 
+        theta = vertical effective stress (positive for increase)
+        :param pw: 62.42796529 lb/ft^3
+        :param g: 32.17405 ft/sec^2
+        :param h:
+        :return:
+        """
+        theta = np.zeros((nlay))
+        for lay in range(nlay):
+            print(lay)
+            theta[lay] = -Pw * g * drawdownGrid[lay]
+        return theta
 
+    theta = del_Estress(drawdownGrid, Pw=62.42796529, g=240177996288.)
+    pc_dd = []
+    for lay in range(nlay):
+        pc_dd = z[lay] - HC
 
+    theta_pc = del_Estress(drawdownGrid=pc_dd, Pw=62.42796529, g=240177996288.)  # preconsolidation
+    b = []
+    for lay in range(nlay):
+        for i in range(len(drawdownGrid)):
+            for Sk in range(len(LN)):
+                if theta[lay][i] < theta_pc[lay]:
+                    S_k = Sfe[lay][Sk]
+                elif theta[lay][i] >= theta_pc[lay]:
+                    S_k = Sfv[lay][Sk]
+                b.append(S_k * drawdownGrid[i])
+    comp = []
+    b = np.array(b)
+    for lay in range(nlay):
+        comp.append(np.zeros((len(b[0]))))
+        comp[lay] += b[lay] * LN[lay]
+
+    return compGrid
 
 
 
